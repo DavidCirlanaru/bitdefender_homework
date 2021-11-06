783ca0c3e3551362931ff977b69f654ebb66f3fb
cirla <cirla@DESKTOP-0ANVRKP>
2021-11-06 12:54:23 +0200

    added dummy functions for unit tests

---
+++ cypress/integration/unit-testing/unit-testing-app.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+// math exports a default object with methods
+import math from "../../../src/math.js";
+
+describe("Unit Test Application Code", function () {
+  const { add, divide, multiply, subtract } = math;
+
+  before(() => {
+    // check if the import worked correctly
+    expect(add, "add").to.be.a("function");
+  });
+
+  context("math.js", function () {
+    it("can add numbers", function () {
+      expect(add(1, 2)).to.eq(3);
+    });
+
+    it("can subtract numbers", function () {
+      expect(subtract(5, 12)).to.eq(-7);
+    });
+
+    it("can divide numbers", function () {
+      expect(divide(27, 9)).to.eq(3);
+    });
+
+    it("can muliple numbers", function () {
+      expect(multiply(5, 4)).to.eq(20);
+    });
+  });
+});
